4bb10d755c491313391a74452f76aa93046a0199
oware1-unbreakable <wareomkar12355@gmail.com>
2026-04-30 13:07:29 +0530

    files added

--- var.java
+++ var.java
@@ -5,8 +5,26 @@ class day2{
         System.out.println("num1="+num1+"success");
         System.out.println("num2="+num2+"success");
 
-        byte num3=128;
+        byte num3=127;
         System.out.println(num3);
+
+        short sh=32767;
+        System.out.println(sh);
+
+        int nuq=214748364;
+        System.out.println(nuq);
+
+        long ter=8796l;
+        System.out.println(ter);
+
+        float gera=6.78f;
+        System.out.println(gera);
+
+        double freak=6.798;
+        System.out.println(freak);
+
+        boolean b=true;
+        System.out.println(b);
 
     }
 }
