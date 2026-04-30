81fcc54c590611a3a6d187404e48c61636784187
Prasanna0102 <pdongre501@gmail.com>
2026-04-30 12:26:59 +0530

    This is variables demo for java17

---
+++ hello.java
@@ -0,0 +1,5 @@
+class my{
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
---
+++ var.java
@@ -0,0 +1,12 @@
+class day2{
+    public static void main(String[] args){
+        int num1=5;
+        int num2=6;
+        System.out.println("num1="+num1+"success");
+        System.out.println("num2="+num2+"success");
+
+        byte num3=128;
+        System.out.println(num3);
+
+    }
+}
