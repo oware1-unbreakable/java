413dd6a6776de7265b6f0e3005e6dd2eff263ecc
oware1-unbreakable <wareomkar12355@gmail.com>
2026-05-06 13:27:25 +0530

    added new file for the casting example

---
+++ cast.java
@@ -0,0 +1,13 @@
+public class cast {
+    public static void main(String[] args){
+        int a = 567;
+        double b=a;
+        System.out.println(b);
+
+        byte c=123;
+        int d=(int) c;
+        System.out.println(d);
+    }
+    
+}
+
